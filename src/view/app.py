fab7f31dc5303d84081030952f1a585ef60fa2bd
Julien <julien.ducrot.mess@gmail.com>
2017-06-28 19:57:57 +0200

    Project setup

---
+++ src/view/app.py
@@ -0,0 +1,12 @@
+import pygame
+from pygame.locals import *
+
+pygame.init()
+win = pygame.display.set_mode((640,400))
+
+continuer = 1
+while continuer:
+        for event in pygame.event.get():
+                if event.type == QUIT:
+                        continuer = 0
+~                                        
