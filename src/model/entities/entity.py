bf0c4f79f1d0d21e299227308ebf39622973489d
Julien <julien.ducrot.mess@gmail.com>
2017-06-28 20:49:56 +0200

    Entity and Coord classes created

---
+++ src/model/entities/entity.py
@@ -0,0 +1,10 @@
+class Entity(object):
+
+    def __init__(self,coords,width,height):
+        super(Entity, self).__init__()
+        self.coords = coords
+        self.width = width
+        self.height = height
+
+    def update():
+        print "Entity update"
